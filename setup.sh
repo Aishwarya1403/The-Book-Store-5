9afe58a0449507ebcfc966cc6080f36de0ce0a60
Aishwarya <a.srivastava1403@gmail.com>
2020-08-03 00:31:19 +0530

    first

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN="dev-b9uuiwyh.us.auth0.com"
 export API_AUDIENCE="https://bookshop/"
 export ALGORITHMS=["RS256"]
-export DATABASE_URL="postgres://ajbsuvfhihwqic:5cdbdfe5543ddf37a49316aa6d4e4f15cd04caa090ef6f457a3cd427950e64d5@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d9mstc2gnobeh3"
+export DATABASE_URL="postgres://ywowhihrzmukcf:4594224f71dbf9133474f7d52e5333eb30fdcd0c69a305f2ea77bfaa0317daf3@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d9torsb033gedu"
