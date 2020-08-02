02f0aa68763b2e98d197211100b72a66b9553c2c
Aishwarya <a.srivastava1403@gmail.com>
2020-08-03 01:24:15 +0530

    second

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import json
 from sqlalchemy.orm import relationship
 
 database_name = "bookstore"
-database_path = "postgres://lxzfaahnzpgmcz:c0df6f3a1aa98a79acf59eb4c7d3fad6a14c985eaf31e345b69ae85c0b94870a@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dbjt20u06sgrr2"
+database_path = "postgres://taofajfsnccdjr:851bab43bc7215395bacf1a183849f5f74d0d48609b77dfb0ec469046c0e345d@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d2p0mft6mv0es2"
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN="dev-b9uuiwyh.us.auth0.com"
 export API_AUDIENCE="https://bookshop/"
 export ALGORITHMS=["RS256"]
-export DATABASE_URL="postgres://lxzfaahnzpgmcz:c0df6f3a1aa98a79acf59eb4c7d3fad6a14c985eaf31e345b69ae85c0b94870a@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dbjt20u06sgrr2"
+export DATABASE_URL="postgres://taofajfsnccdjr:851bab43bc7215395bacf1a183849f5f74d0d48609b77dfb0ec469046c0e345d@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d2p0mft6mv0es2"
