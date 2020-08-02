c3b65abd0e5961859ca0bf07fcce521a87a1bb88
Aishwarya <a.srivastava1403@gmail.com>
2020-08-03 02:49:35 +0530

    changed-url

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import json
 from sqlalchemy.orm import relationship
 
 database_name = "bookstore"
-database_path = "postgres://postgres@localhost:5432/bookstore"
+database_path = "postgres://zlqnqaggmdnzlu:85cb252edd9cab2a85ff5295d88006bc34f32d0ef3e67f8192421664f52ee0ca@ec2-52-1-95-247.compute-1.amazonaws.com:5432/d76g8nijpsoav2"
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN="dev-b9uuiwyh.us.auth0.com"
 export API_AUDIENCE="https://bookshop/"
 export ALGORITHMS=["RS256"]
-export DATABASE_URL="postgres://postgres@localhost:5432/bookstore"
+export DATABASE_URL="postgres://zlqnqaggmdnzlu:85cb252edd9cab2a85ff5295d88006bc34f32d0ef3e67f8192421664f52ee0ca@ec2-52-1-95-247.compute-1.amazonaws.com:5432/d76g8nijpsoav2"
